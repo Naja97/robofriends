df441dd1b4ddc24f006436b3af526da2337b7d8d
Jana <jana.programmer@gmail.com>
2022-11-23 19:39:38 +0100

    Updates

---
+++ precache-manifest.4f8168c577043ce35d1f6cd24e5f809a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "49267a32f7dd10b1ed55",
+    "url": "/robofriends/static/css/main.6e731275.chunk.css"
+  },
+  {
+    "revision": "49267a32f7dd10b1ed55",
+    "url": "/robofriends/static/js/main.49267a32.chunk.js"
+  },
+  {
+    "revision": "84cf99745ce0980187aa",
+    "url": "/robofriends/static/js/1.84cf9974.chunk.js"
+  },
+  {
+    "revision": "b5456caa0cf5e155a208",
+    "url": "/robofriends/static/css/2.3547d278.chunk.css"
+  },
+  {
+    "revision": "b5456caa0cf5e155a208",
+    "url": "/robofriends/static/js/2.b5456caa.chunk.js"
+  },
+  {
+    "revision": "6bf665704170461feaf8",
+    "url": "/robofriends/static/js/runtime~main.6bf66570.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "b87ae1a549f24cf9925c48186aa5ee87",
+    "url": "/robofriends/index.html"
+  }
+];
